f0f5013c556cdd795962153ffdf156ca4fb04f3e
Kolton <kguthrie2@stu.jsu.edu>
2025-01-23 13:28:51 -0600

    Commit

---
+++ app/src/main/java/com/jsu/cs408/lab2b/MainActivity.java
@@ -0,0 +1,25 @@
+package com.jsu.cs408.lab2b;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.jsu.cs408.lab2b.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+}
